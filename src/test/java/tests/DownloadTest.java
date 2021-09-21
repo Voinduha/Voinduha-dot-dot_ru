230761bc96c41aa11538ae5a26ee5a7ca14ce015
danvu <danvu@ya.ru>
2021-09-21 18:20:50 +0300

    added changes to pdf file

--- src/test/java/tests/DownloadTest.java
+++ src/test/java/tests/DownloadTest.java
@@ -1,18 +1,22 @@
 package tests;
 
+import com.codeborne.pdftest.PDF;
 import com.codeborne.selenide.Configuration;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static Utils.Files.readTextFromFile;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.files.FileFilters.withExtension;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 public class DownloadTest {
@@ -21,12 +25,13 @@ public class DownloadTest {
     @Description("Soon to be implemented by me (or QA engineers)")
     @DisplayName("Загружаем файл с презентацией компании")
     void presentationDownload() throws IOException {
-        Configuration.downloadsFolder = "downloads";
 
-        open("https://dot-dot.ru/media/download/7566");
-        File downloadedFile = $("about__link").download(withExtension("htm"));
-        String fileContent = readTextFromFile(downloadedFile);
-        assertThat(fileContent, containsString("Презентация"));
+
+        open("https://dot-dot.ru/aboutus");
+        File download = $(byText("Презентация")).download();
+        PDF parsed = new PDF(download);
+        assertThat(parsed.text).contains("Предложение");
+
     }
 }
 
