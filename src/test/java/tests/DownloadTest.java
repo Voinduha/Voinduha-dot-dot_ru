614d96c7e93a348d22aacdab6339b3390bb1ac76
danvu <danvu@ya.ru>
2021-09-21 18:23:42 +0300

    added changes to pdf file

--- src/test/java/tests/DownloadTest.java
+++ src/test/java/tests/DownloadTest.java
@@ -7,17 +7,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
-import static Utils.Files.readTextFromFile;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
 
 public class DownloadTest {
 
@@ -25,7 +20,7 @@ public class DownloadTest {
     @Description("Soon to be implemented by me (or QA engineers)")
     @DisplayName("Загружаем файл с презентацией компании")
     void presentationDownload() throws IOException {
-
+        Configuration.downloadsFolder = "downloads";
 
         open("https://dot-dot.ru/aboutus");
         File download = $(byText("Презентация")).download();
